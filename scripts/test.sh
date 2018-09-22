d17de17166420819c16f1c0b26f0a913f692cafb
trumae <trumae@gmail.com>
2018-09-22 19:51:17 -0300

    test erase

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@
 source ./setup.sh
 
 cleos -u http://${NODE_EOS}:8800 --wallet-url http://${NODE_EOS}:${PORT} push action eosdatavault add '["player1","datadatadatadata","pass"]' -p player1
-
 cleos -u http://${NODE_EOS}:8800 --wallet-url http://${NODE_EOS}:${PORT} push action eosdatavault add '["player1","datadatadatadata2","pass2"]' -p player1
-
 cleos -u http://${NODE_EOS}:8800 --wallet-url http://${NODE_EOS}:${PORT} push action eosdatavault add '["player2","datadatadatadata3","pass3"]' -p player2
+
+cleos -u http://${NODE_EOS}:8800 --wallet-url http://${NODE_EOS}:${PORT} push action eosdatavault erase '["player1", 0]' -p player1
